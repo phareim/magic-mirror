ad3484a61619ca3cc6ebbad0356aad24cf335a8c
Petter Hareim <petter.hareim@bouvet.no>
2017-01-27 23:37:14 +0100

    fix emo_camera

--- modules/emo_camera/camera.js
+++ modules/emo_camera/camera.js
@@ -1,191 +1,191 @@
-
 'use strict';
 
 
-Module.register("camera", {
+Module.register("emo_camera", {
 
-    counter: null,
-	// Default module config.
-	defaults: {
-        selfieInterval: 3,
-		emailConfig: {
-			service: 'Hotmail',
-			auth: {
-				user: '<name@email.com>',
-				pass: '<password>'
-			}
-		}
-	},
-    
-	display: true,
+  counter: null,
+  // Default module config.
+  defaults: {
+    selfieInterval: 3,
+    emailConfig: {
+      service: 'Hotmail',
+      auth: {
+        user: '<name@email.com>',
+        pass: '<password>'
+      }
+    }
+  },
 
-	cameraPreview: null,
-	snapshot: null,
-	camera: null,
-	image: null,
-	processing: false,
-	message: null,
-	commands: null,
+  display: true,
 
-    getScripts: function() {
-		return ["webcam.js"];
-	},
+  cameraPreview: null,
+  snapshot: null,
+  camera: null,
+  image: null,
+  processing: false,
+  message: null,
+  commands: null,
 
-    start: function() { 
-		this.message = "Say 'SELFIE' to make a selfie or 'HIDE CAMERA' to hide";
-        this.sendSocketNotification('INIT_MAILER', this.config);
-		 console.log('making selfie');
-		this.makeSelfie();
-    },
+  getScripts: function() {
+    return ["webcam.js"];
+  },
 
-	makeSelfie: function(){
-		var self = this;
-		var timer = 1;
-		var interval = setInterval(function() {
-			if (timer === 4) {
-				clearInterval(interval);
-				self.createSnapshot();
-			}
-			else {
-				self.counter.innerHTML = timer;
-				timer++;
-			}
-		}, 1000);
-	},
+  start: function() {
+    this.message =
+      "Hei!";
+    this.sendSocketNotification('INIT_MAILER', this.config);
+    console.log('making selfie');
+    this.makeSelfie();
+  },
 
-	createSnapshot: function() {
-		var self = this;
-		this.processing = true;
-		Webcam.snap(function (data_uri, canvas, context) {
-          var data = data_uri;
-		  if (self.image == null){
-			    self.image = document.createElement("img");
-				
-				self.image.width = 640;
-				self.image.height = 480;
-				self.snapshot.appendChild(self.image);
-		  }
-		  self.image.src = data_uri;
-		
-		   var file = self.image.src;
-		   
-		   
-            var BASE64_MARKER = ';base64,';
-           
-            var parts = file.split(BASE64_MARKER);
-            var contentType = parts[0].split(':')[1];
-            var raw = window.atob(parts[1]);
-            var rawLength = raw.length;
+  makeSelfie: function() {
+    var self = this;
+    var timer = 1;
+    var interval = setInterval(function() {
+      if (timer === 4) {
+        clearInterval(interval);
+        self.createSnapshot();
+      } else {
+        self.counter.innerHTML = timer;
+        timer++;
+      }
+    }, 1000);
+  },
 
-            var uInt8Array = new Uint8Array(rawLength);
+  createSnapshot: function() {
+    var self = this;
+    this.processing = true;
+    Webcam.snap(function(data_uri, canvas, context) {
+      var data = data_uri;
+      if (self.image == null) {
+        self.image = document.createElement("img");
 
-            for (var i = 0; i < rawLength; ++i) {
-                uInt8Array[i] = raw.charCodeAt(i);
-            }
+        self.image.width = 640;
+        self.image.height = 480;
+        self.snapshot.appendChild(self.image);
+      }
+      self.image.src = data_uri;
 
-            file= new Blob([uInt8Array], { type: contentType });
-        
-
-		   var http = new XMLHttpRequest();
-var url = "https://api.projectoxford.ai/emotion/v1.0/recognize";
-http.open("POST", url, true);
-//http.setRequestHeader("ocp-apim-subscription-key":"2b12ee0ed92b4fa18d3009b7370b448f");
-http.setRequestHeader("ocp-apim-subscription-key", "2b12ee0ed92b4fa18d3009b7370b448f");
-http.setRequestHeader("Content-type", "application/octet-stream");
-http.setRequestHeader("postman-token", "70c8f649-c68b-c086-e134-2da97ad86589");
+      var file = self.image.src;
 
 
-http.send(file);
-		   
-		   
-    
-		
-		
-		
-		
-		  self.cameraPreview.style.display = 'none';
-		  self.sendSocketNotification('SEND_EMAIL',  { config: self.config, dataUrl:data_uri } );
-		  self.commands.innerHTML = "Your selfie will be emailed to you";
-		  setTimeout(function(){
-			  self.commands.innerHTML = self.message;
-		  }, 3000);
-		  self.processing = false;
-		});
-	},
+      var BASE64_MARKER = ';base64,';
 
-	// Override dom generator.
-	getDom: function() {
+      var parts = file.split(BASE64_MARKER);
+      var contentType = parts[0].split(':')[1];
+      var raw = window.atob(parts[1]);
+      var rawLength = raw.length;
 
-        var wrapper = document.createElement("div");
+      var uInt8Array = new Uint8Array(rawLength);
 
-		if (this.display) {
-			// if (this.camera === null) {
+      for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+      }
 
-				this.camera = document.createElement("div");
-				this.counter = document.createElement("div")
-				this.counter.style = "text-align: center; padding-bottom: 10px;";
-				this.counter.className = "large normal";
+      file = new Blob([uInt8Array], {
+        type: contentType
+      });
 
-				this.camera.appendChild(this.counter);
-				this.cameraPreview = document.createElement("div");
-				this.camera.appendChild(this.cameraPreview);
-				this.snapshot = document.createElement("div");
-				this.camera.appendChild(this.snapshot)
-				this.commands = document.createElement("div");
-				this.commands.innerHTML = this.message;
-				this.commands.className = "small light dimmed";
-				this.commands.style = "padding-top: 10px;"
-				this.camera.appendChild(this.commands);
 
-				wrapper.appendChild(this.camera);
+      var http = new XMLHttpRequest();
+      var url = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+      http.open("POST", url, true);
+      //http.setRequestHeader("ocp-apim-subscription-key":"2b12ee0ed92b4fa18d3009b7370b448f");
+      http.setRequestHeader("ocp-apim-subscription-key",
+        "2b12ee0ed92b4fa18d3009b7370b448f");
+      http.setRequestHeader("Content-type",
+        "application/octet-stream");
 
-				Webcam.set({
-					width: 640,
-					height: 480,
-					image_format: 'jpeg',
-					jpeg_quality: 90,
-					constraints: {
-						mandatory: {
-							minWidth: 640,
-							minHeight: 480
-						},
-						optional: [
-							{ minFrameRate: 60 }
-						]
-					}
-				});
 
-				Webcam.attach(this.cameraPreview);
+      http.send(file);
 
-		}
-		else {
-			if (this.camera != null){
-				Webcam.reset();
-				this.camera.style = "visibility:hidden;";
-			}
-			
-		}
-		
-        return wrapper;
-	},
 
-    notificationReceived: function(notification, payload, sender) {
-		if (notification === "SHOW_CAMERA" && this.display === false){
-			this.display = true;
-			this.updateDom(500);
-		}
 
-        if (notification === "HIDE_CAMERA" && this.display == true){
-			this.display = false;
-			this.updateDom(500);
-		}
+      self.cameraPreview.style.display = 'none';
+      self.sendSocketNotification('SEND_EMAIL', {
+        config: self.config,
+        dataUrl: data_uri
+      });
+      self.commands.innerHTML = "Your selfie will be emailed to you";
+      setTimeout(function() {
+        self.commands.innerHTML = self.message;
+      }, 3000);
+      self.processing = false;
+    });
+  },
 
-        if (notification === "SELFIE"){
-			if (!this.processing && this.display){
-				this.makeSelfie();
-			}
-		}
+  // Override dom generator.
+  getDom: function() {
 
-	},
+    var wrapper = document.createElement("div");
+
+    if (this.display) {
+      // if (this.camera === null) {
+
+      this.camera = document.createElement("div");
+      this.counter = document.createElement("div")
+      this.counter.style = "text-align: center; padding-bottom: 10px;";
+      this.counter.className = "large normal";
+
+      this.camera.appendChild(this.counter);
+      this.cameraPreview = document.createElement("div");
+      this.camera.appendChild(this.cameraPreview);
+      this.snapshot = document.createElement("div");
+      this.camera.appendChild(this.snapshot)
+      this.commands = document.createElement("div");
+      this.commands.innerHTML = this.message;
+      this.commands.className = "small light dimmed";
+      this.commands.style = "padding-top: 10px;"
+      this.camera.appendChild(this.commands);
+
+      wrapper.appendChild(this.camera);
+
+      Webcam.set({
+        width: 640,
+        height: 480,
+        image_format: 'jpeg',
+        jpeg_quality: 90,
+        constraints: {
+          mandatory: {
+            minWidth: 640,
+            minHeight: 480
+          },
+          optional: [{
+            minFrameRate: 60
+          }]
+        }
+      });
+
+      Webcam.attach(this.cameraPreview);
+
+    } else {
+      if (this.camera != null) {
+        Webcam.reset();
+        this.camera.style = "visibility:hidden;";
+      }
+
+    }
+
+    return wrapper;
+  },
+
+  notificationReceived: function(notification, payload, sender) {
+    if (notification === "SHOW_CAMERA" && this.display === false) {
+      this.display = true;
+      this.updateDom(500);
+    }
+
+    if (notification === "HIDE_CAMERA" && this.display == true) {
+      this.display = false;
+      this.updateDom(500);
+    }
+
+    if (notification === "SELFIE") {
+      if (!this.processing && this.display) {
+        this.makeSelfie();
+      }
+    }
+
+  },
 
 });
