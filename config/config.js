a58fd8f5ddf6c0f90e8afe2b4f0cfaf34c3de21e
Petter Hareim <petter.hareim@bouvet.no>
2017-01-28 01:21:54 +0100

    add swipe

--- config/config.js
+++ config/config.js
@@ -13,67 +13,64 @@ var config = {
 	units: 'metric',
 
 	modules: [{
-			module: 'alert',
+		module: 'alert',
+	}, {
+		module: 'MMM-Swipe',
+		position: 'bottom_left', // Doesn't matter after it's setup.  It should be blank.
+		// Best results in one of the side regions like: bottom_left
+		config: {
+			// See 'Configuration options' for more information.
+			echoLeftPin: 24, //Left Sensor's BCM Numbered Echo pin - REQUIRED
+			triggerLeftPin: 23, //Left Sensor's BCM Numbered trigger pin - REQUIRED
+			echoRightPin: 26, //Right Sensor's BCM Numbered Echo pin - REQUIRED
+			triggerRightPin: 25, //Right Sensor's BCM Numbered trigger pin - REQUIRED
+			useAsButton: false, //Enable a GPIO output when you "press".
+			buttonPin: 8,
+			verbose: false,
+			calibrate: false
 		}
-		/*, {
-				module: 'MMM-Swipe',
-				position: 'bottom_left', // Doesn't matter after it's setup.  It should be blank.
-				// Best results in one of the side regions like: bottom_left
-				config: {
-					// See 'Configuration options' for more information.
-					echoLeftPin: 24, //Left Sensor's BCM Numbered Echo pin - REQUIRED
-					triggerLeftPin: 23, //Left Sensor's BCM Numbered trigger pin - REQUIRED
-					echoRightPin: 26, //Right Sensor's BCM Numbered Echo pin - REQUIRED
-					triggerRightPin: 25, //Right Sensor's BCM Numbered trigger pin - REQUIRED
-					useAsButton: false, //Enable a GPIO output when you "press".
-					buttonPin: 8,
-					verbose: false,
-					calibrate: false
-				}
-			}*/
-		, {
-			module: "updatenotification",
-			position: "top_bar"
-		}, {
-			module: 'clock',
-			position: 'top_left'
-		}, {
-			module: 'hello',
-			position: 'bottom_left'
-		}, {
-			module: 'compliments',
-			position: 'center',
-			config: {
-				getEmotion: function() {
-					return _emotion;
-				}
-			}
-		}, {
-			module: 'currentweather',
-			position: 'top_right',
-			config: {
-				location: 'Stavanger',
-				locationID: '3137115',
-				appid: '648212d459dde8390c2a03a5f617ad23'
-			}
-		}, {
-			module: 'emo_camera',
-			position: 'lower_third',
-			selfieInterval: 3, // Time interval in seconds before the photo will be taken.
-			config: {
-				setEmotion: function(emotion) {
-					_emotion = emotion;
-				}
-			},
-			emailConfig: {
-				service: 'Hotmail', // Email provider to use to send email with a photo.
-				auth: {
-					user: 'siljehetland@hotmail.com', // Your email account
-					pass: '' // Your password for email account
-				}
+	}, {
+		module: "updatenotification",
+		position: "top_bar"
+	}, {
+		module: 'clock',
+		position: 'top_left'
+	}, {
+		module: 'hello',
+		position: 'bottom_left'
+	}, {
+		module: 'compliments',
+		position: 'center',
+		config: {
+			getEmotion: function() {
+				return _emotion;
 			}
 		}
-	]
+	}, {
+		module: 'currentweather',
+		position: 'top_right',
+		config: {
+			location: 'Stavanger',
+			locationID: '3137115',
+			appid: '648212d459dde8390c2a03a5f617ad23'
+		}
+	}, {
+		module: 'emo_camera',
+		position: 'lower_third',
+		selfieInterval: 3, // Time interval in seconds before the photo will be taken.
+		config: {
+			setEmotion: function(emotion) {
+				_emotion = emotion;
+			}
+		},
+		emailConfig: {
+			service: 'Hotmail', // Email provider to use to send email with a photo.
+			auth: {
+				user: 'siljehetland@hotmail.com', // Your email account
+				pass: '' // Your password for email account
+			}
+		}
+	}]
 
 };
 
